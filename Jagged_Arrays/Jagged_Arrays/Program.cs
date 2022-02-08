1cb22206f9f7a60b0ca61dab627269899ef5befa
webtracer <74070641+webtracer@users.noreply.github.com>
2022-02-08 09:25:43 -0800

    Add files via upload

---
+++ Jagged_Arrays/Jagged_Arrays/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Jagged_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[][] jaggedArray = new int[3][];
+
+            //Can initialize this way - specifying the values but empty
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[3];
+            jaggedArray[2] = new int[2];
+            //Or initialize this way - with values instead of specifying like above
+            jaggedArray[0] = new int[] { 2, 3, 5, 7, 11 };
+            jaggedArray[1] = new int[] { 1, 2, 3 };
+            jaggedArray[2] = new int[] { 13, 21 };
+
+            //Alternative way
+            int[][] jaggedArray2 = new int[][]
+            {
+                new int[] { 2, 3, 5, 7, 11 },
+                new int[] { 1, 2, 3 },
+                new int[] { 13, 21 }
+            };
+
+            Console.WriteLine($"The value in the middle of the first entry is {jaggedArray2[0][2]}");
+
+            // My attempt - which almost was on track
+            /*int[] counter = { 0 };
+            for(int i = 0, j = 0; i < jaggedArray2.GetLength(0); i++,j++)
+            {
+                //for (int j = 0; j < jaggedArray2.GetLength(0); j++)
+                //{
+                    Console.Write($"The contents of the array are: ");
+                    Console.WriteLine(jaggedArray2[j][i]);
+                //}
+            }*/
+
+            // The solution
+            for(int i =0; i < jaggedArray2.Length; i++)
+            {
+                Console.WriteLine($"Element {i}");
+                for(int j = 0; j < jaggedArray2[i].Length; j++)
+                {
+                    Console.WriteLine(jaggedArray2[i][j]);
+                }
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Jagged_Arrays_Challenge/Jagged_Arrays_Challenge/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Jagged_Arrays_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a jagged array, which contains 3 "friends arrays", in which two family members should be stored
+            // Introduce family members from different families to each other via console (3 introductions)
+
+            string[][] familyArray = new string[][]
+            {
+                new string[] {"Niel", "Terry"},
+                new string[] {"Art","Joanne"},
+                new string[] {"Vickie", "Monica"}
+            };
+
+            Console.WriteLine($"Introducing the families to each other");
+
+            // My solution - I made it WAY too difficult, thought of his solution but figured it was too easy
+            for(int i = 0; i < familyArray.Length;i++)
+            {
+                for(int j = 0; j < familyArray[i].Length;j++)
+                {
+                    //Console.WriteLine($"{familyArray[i][j]} I would like you to meet {familyArray[j][i]}");
+                    for (int k = 0; k < familyArray[i].Rank;k++)
+                    {
+                        Console.WriteLine($"{familyArray[j][k]} I would like you to meet {familyArray[i][j]}");
+                    }
+                }
+            }
+            Console.WriteLine();
+            /*for(int i=0,j=1,k=0; i<familyArray.Length;i++,j--,k++)
+            {
+                Console.WriteLine($"{familyArray[i][k]} I would like you to meet {familyArray[i][j]}");
+            }*/
+
+            // Actual Solution - I made it WAY to difficult
+            string[][] friendsAndFamliy = new string[][]
+            {
+                new string[] {"Niel", "Terry"},
+                new string[] {"Art","Joanne"},
+                new string[] {"Vickie", "Monica"}
+            };
+
+            Console.WriteLine($"Hi {friendsAndFamliy[0][0]}, I would like to introduce {friendsAndFamliy[1][0]} to you.");
+            Console.WriteLine($"Hi {friendsAndFamliy[0][1]}, I would like to introduce {friendsAndFamliy[1][1]} to you.");
+            Console.WriteLine($"Hi {friendsAndFamliy[2][0]}, I would like to introduce {friendsAndFamliy[2][1]} to you.");
+            Console.WriteLine();
+            // Can also create jagged arrays this way
+            string[] laurasParents = new string[] { "Niel", "Terry" };
+            string[] randysParents = new string[] { "Art", "Joanne" };
+            string[] ourAunts = new string[] { "Vickie", "Monica" };
+
+            string[][] familyNames = new string[][]
+            {
+                laurasParents,
+                randysParents,
+                ourAunts
+            };
+
+            Console.WriteLine($"Hi {familyNames[0][0]}, I would like to introduce {familyNames[1][0]} to you.");
+            Console.WriteLine($"Hi {familyNames[0][1]}, I would like to introduce {familyNames[1][1]} to you.");
+            Console.WriteLine($"Hi {familyNames[2][0]}, I would like to introduce {familyNames[2][1]} to you.");
+
+        }
+    }
+}
